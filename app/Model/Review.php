1ec03c7a2f13e2cfde580bbb06e7c16f004db8c4
JaliyaLaptop <jaliyalamahewa@gmail.com>
2017-12-03 09:36:27 +0530

    product model and database seeds

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+     public function review(){
+    	return $this->belongsTo('App\Model\Product');
+    }
+
 }
---
+++ database/factories/ModelProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Model\Product::class, function (Faker\Generator $faker) {
+  
+    return [
+        'name' => $faker->word,
+        'detail' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
